66b699a92d1a444d0f7efbc21d7c852807c703af
bmurdata <bpmjoker@gmail.com>
2021-06-28 22:42:52 -0400

    Update Flask code, experiment with data

--- mongoSearch/findLocal.py
+++ mongoSearch/findLocal.py
@@ -1,34 +1,45 @@
+# Tired and will fix up later. More of a playground to learn Mongo, Flask Pymongo, and other stuff.
+from weakref import ProxyTypes
 import dbLocal2
 db=dbLocal2.db
 monConnection=dbLocal2.monConnection
 from bson import json_util
 import json
-import pymongo
+# import pymongo
 
-metaJobs="jobInfo_Meta"
-client = pymongo.MongoClient(monConnection)
-database=client[db]
+from flask_pymongo import PyMongo
+from flask import Flask, jsonify
+from bson.json_util import dumps, loads
+app = Flask(__name__)
+# client = pymongo.MongoClient(monConnection)
+app.config["MONGO_URI"]=monConnection
+
+database=PyMongo(app)
 def showItems(myCollection:str()):
-    collec=database[myCollection]
-    for item in collec.find():
-        print(type(item))
-        print(item)
+    collec=database.db.jobsByCode.find()
+    docs=[doc for doc in collec]
+    return docs
+    return loads(dumps(collec))
+
+jobsByCode="jobsByCode"
 metaCollection="jobInfo_Meta"
 contentCollection="jobInfo_Content"
-# showItems(metaCollection)
 
-# json.dumps(result,default=json_util.default)
-from flask import Flask
-app = Flask(__name__)
 def showOne(myCollection:str()):
-    collec=database[myCollection]
-    for item,val in enumerate(collec.find()):
-        mydict=item
-        print(val)
-    
-    #mydict=collec.find_one()
-
-    return json.dumps(mydict)
+    collec=database.db.jobsByCode.find_one_or_404()
+    print(collec)
+    return collec
+# print(showItems(contentCollection))
 @app.route('/')
-def hello_world():
-    return showOne(contentCollection)
+def index():
+    jobs=database.db.jobsByCode.find({},{'_id': False})
+    m={'cursor': [job for job in jobs]}
+    # m= [job for job in jobs]
+    return dumps(m)
+@app.route('/jobs')
+def jobs():
+    return jsonify(showItems(jobsByCode))
+
+
+if __name__=="__main__":
+    app.run()
---
+++ mongoSearch/models/jobInfo_Content.py
@@ -0,0 +1,1 @@
+# Place Job Info content
