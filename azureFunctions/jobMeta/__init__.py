5b76c4e635830e60405f63239e03798e535578d2
bmurdata <32182553+bmcap@users.noreply.github.com>
2021-07-02 12:55:09 -0400

    Update jobMeta Function

--- azureFunctions/jobMeta/__init__.py
+++ azureFunctions/jobMeta/__init__.py
@@ -1,56 +1,40 @@
 import logging
 import azure.functions as func
 import pymongo
-import json
 from bson.json_util import dumps
 import dbLocal2 as dbsetup
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
+    logging.info('Python jobMeta trigger function processed a request.')
     searchCollection="jobInfo_Meta"
-    code = req.params.get('jnum')
-    if not code:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('jnum')
+    allMatch="[A-Z]"
+    code =  req.params.get('jnum')  if req.params.get('jnum') else "[0-9]"
+    title= req.params.get('btitle')  if req.params.get('btitle') else allMatch
+    cstitle= req.params.get('cstitle')  if req.params.get('cstitle') else allMatch
+    longCat= req.params.get('hname')  if req.params.get('hname') else allMatch
+ 
     if code:
+        logging.info("Found a code")
         try:
             url = dbsetup.monConnection
             client = pymongo.MongoClient(url)
             database = client[dbsetup.db]
             collection = database[searchCollection]
+            myConditions=[
+                {"HiringAgency":{"$regex":str(longCat),"$options":"-i"}},
+                {"jobNum":{"$regex":str(code),"$options":"-i"}},
+                {"Business Title":{"$regex":str(title),"$options":"-i"}},
+                {"Civil Service Title":{"$regex":str(cstitle),"$options":"-i"}},
+                ]
+            result = collection.find({'$and':myConditions},{"_id":False})
 
-            result = collection.find({"jobNum":str(code)},{"_id":False})
-            
             result = dumps(result)
-            logging.info("Processing result")
             logging.info(len(result))
-            m=list(result)
-            logging.info(len(m))
-        except:
-            result="[{'Error':'No Result by that code. Enter numerical value. Example: code=12345'}]"
-            logging.info("Error")
+
+        except Exception as e:
+            result="[{'Error':'No Result by "+str(code)+ "code. Enter numerical value. Example: code=12345'}]"
+            logging.info("Error below:")
+            logging.info(e)
         if len(result)==2:
-            result="[{'Error':'No Result by that code. Enter numerical value. Example: code=12345'}]"
-        return func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
-    else:
-        try:
-            url = dbsetup.monConnection
-            client = pymongo.MongoClient(url)
-            database = client[dbsetup.db]
-            collection = database[searchCollection]
-
-            result = collection.find({},{"_id":False})
-            
-            result = dumps(result)
-            logging.info("Processing result")
-            logging.info(len(result))
-            m=list(result)
-            logging.info(len(m))
-        except:
-            result="[{'Error':'No Result}]"
-            logging.info("Error")
+            result="[{'Error':'No Result by that code "+str(code)+". Enter numerical value. Example: code=12345'}]"
         return func.HttpResponse(result, mimetype="application/json", charset='utf-8', status_code=200)
